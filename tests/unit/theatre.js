83d46e74a30f7e28392c8b3fdac2cd175fb1264b
Jonathan Pollack <jonathanpollack@users.noreply.github.com>
2016-06-17 10:51:19 +0200

    Move tests into tests/

---
+++ tests/unit/theatre.js
@@ -0,0 +1,12 @@
+var fs = require('fs')
+var cheerio = require('cheerio')
+var Theatre = require('../../index.js').Theatre
+
+let testHtml = fs.readFileSync('./tests/fixtures/hinsdale_mt_showtimes.html').toString()
+let $ = cheerio.load(testHtml.replace(/(\n|\s{2,})/g, ' '))
+
+let theatres = $('.theater')
+    .toArray()
+    .map(theatreElement => new Theatre($(theatreElement)))
+
+console.log(theatres)
