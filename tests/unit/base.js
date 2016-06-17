aa09744900d5eacd504bda6d2059ded63dea3ceb
Jonathan Pollack <jonathanpollack@users.noreply.github.com>
2016-06-17 19:51:23 +0200

    Add mocha and chai for unit testing

--- tests/unit/base.js
+++ tests/unit/base.js
@@ -1,23 +1,28 @@
 /* eslint-env mocha */
 import fs from 'fs'
 import cheerio from 'cheerio'
-import Theatre from '../../src/theatre.js'
+import Base from '../../src/base.js'
 import { expect } from 'chai'
 
 const TEST_HTML = fs.readFileSync('./tests/fixtures/hinsdale_mt_showtimes.html', 'utf8').replace(/(\n|\s{2,})/g, ' ')
 let $ = cheerio.load(TEST_HTML)
 const THEATRES = $('.theater').toArray()
 
-describe('Theatre', function () {
+describe('Base', function () {
   describe('constructor', function () {
     it('should take a cheerio element', function () {
-      expect(() => new Theatre($(THEATRES[0]))).to.not.throw(Error)
+      expect(() => new Base($(THEATRES[0]))).to.not.throw(Error)
     })
     it('should throw an error when not given a cheerio element', function () {
-      expect(() => new Theatre(THEATRES[0])).to.throw(Error)
+      expect(() => new Base(THEATRES[0])).to.throw(Error)
     })
-    it('should set name, info, url, id, and showtimes', function () {
-      expect(new Theatre($(THEATRES[0]))).to.include.keys(['name', 'info', 'url', 'id', 'showtimes'])
+    it('should set name, info, url, and id', function () {
+      expect(new Base($(THEATRES[0]))).to.include.keys(['name', 'info', 'url', 'id'])
     })
+  })
+  describe('_stripAndTrim', function () {
+    it('should take a string')
+    it('should remove unicode characters')
+    it('should trim whitespace')
   })
 })
